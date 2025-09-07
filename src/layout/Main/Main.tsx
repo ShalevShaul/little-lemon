408d1cb7c715b978bc2b636a6d87dab6e2f2abff
Shalev Shaul <shalevshaul1@gmail.com>
2025-09-07 13:00:58 +0300

    add lazy loading for better performance

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,24 +1,28 @@
+import { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import PageHeader from '../PageHeader/PageHeader';
 import './Main.css';
-import Home from '../../pages/Home/Home';
-import BookingForm from '../../pages/BookingForm/BookingForm';
-import ExistingBookings from '../../pages/ExistingBookings/ExistingBookings';
-import NotFound from '../../pages/NotFound/NotFound';
-import BookingConfirmation from '../../pages/BookingConfirmation/BookingConfirmation';
+import Loader from '../../components/Loader/Loader';
+const Home = lazy(() => import('../../pages/Home/Home'));
+const BookingForm = lazy(() => import('../../pages/BookingForm/BookingForm'));
+const ExistingBookings = lazy(() => import('../../pages/ExistingBookings/ExistingBookings'));
+const BookingConfirmation = lazy(() => import('../../pages/BookingConfirmation/BookingConfirmation'));
+const NotFound = lazy(() => import('../../pages/NotFound/NotFound'));
 
 function Main() {
     return (
         <main>
             <PageHeader />
-            <Routes>
-                <Route path='/home' element={<Home />} />
-                <Route path='/reserve-a-table' element={<BookingForm />} />
-                <Route path='/bookings' element={<ExistingBookings />} />
-                <Route path='/confirmed' element={<BookingConfirmation />} />
-                <Route path='/' element={<Navigate to='/home' />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+            <Suspense fallback={<Loader />}>
+                <Routes>
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/reserve-a-table' element={<BookingForm />} />
+                    <Route path='/bookings' element={<ExistingBookings />} />
+                    <Route path='/confirmed' element={<BookingConfirmation />} />
+                    <Route path='/' element={<Navigate to='/home' />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </Suspense>
         </main>
     )
 }
