f4c90fa1457c16f8248d1ba68888bf0011b3036a
Shalev Shaul <shalevshaul1@gmail.com>
2025-09-17 17:08:51 +0300

    Add color prop to CustomButton component and implement it with css for every color

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -2,16 +2,19 @@ import './CustomButton.css';
 
 interface CustomButtonProps {
     text: string,
+    color: 'primary' | 'secondary',
     type?: HTMLButtonElement['type'],
     onClick?: () => void
 }
 
-function CustomButton({ text, type, onClick }: CustomButtonProps) {
+function CustomButton({ text, type, color, onClick }: CustomButtonProps) {
     return (
         <button
             type={type || 'button'}
             role='button'
-            className='buttonComponent'
+            className={`buttonComponent
+            ${color === 'primary' ? 'primary' : ''}
+            ${color === 'secondary' ? 'secondary' : ''}`}
             onClick={onClick}
         >
             {text}
